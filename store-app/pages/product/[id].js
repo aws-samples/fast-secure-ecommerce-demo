cccfab48b0e718fcb15be00d8b386c42348a093b
Achraf Souk <48942933+achrafsouk@users.noreply.github.com>
2024-09-15 18:12:12 +0400

    Update [id].js - add metadata

--- store-app/pages/product/[id].js
+++ store-app/pages/product/[id].js
@@ -1,7 +1,8 @@
-
 import Image from 'next/image'
+import Head from 'next/head';
 import { useState } from 'react';
 import { useRouter } from 'next/router';
+
 
 import Layout from './../components/Layout';
 import { getProduct, getComments } from '../../lib/ddb';
@@ -74,63 +75,72 @@ export default function Product({ product, comments, username }) {
   };
 
   return (
-    <Layout username={username}>
-      <div className="bg-white rounded-lg shadow-md overflow-hidden">
-        <div className="p-6">
-          <h1 className="text-3xl font-bold mb-4">{product.name}</h1>
-          <Image src={product.image} alt={product.name} className="w-full h-full object-cover" width={640} height={640} />
-          <p className="text-gray-600 mb-4">{product.description}</p>
-          <p className="text-2xl font-bold text-green-600 mb-4">${product.price}</p>
-          <button className="bg-green-500 text-white px-4 py-2 rounded hover:bg-green-600 transition duration-300" onClick={() => { addItemToCart(product) }}>
-            Add to Cart
-          </button>
-        </div>
-        <div className="p-6">
-          <h1 className="text-3xl font-bold mb-4">Comments</h1>
-          {comments.map((comment) => (
-            <div className="bg-grey rounded-lg shadow-md overflow-hidden hover:shadow-lg transition duration-300 gap-6">
-              <div className="p-4">
-                <h2 className="text-xl font-semibold mb-2">{comment.username}</h2>
-                <p className="text-gray-600 mb-4">{comment.text}</p>
+    <>
+      <Head>
+        <title key="title">{product.name}</title>
+        <meta name="description" content={product.description} key="description" />
+        <meta property="og:title" content={product.name} key="og-title" />
+        <meta property="og:type" content="article" key="og-type" />
+        <meta property="og:description" content={product.description} key="og-description" />
+      </Head>
+      <Layout username={username}>
+        <div className="bg-white rounded-lg shadow-md overflow-hidden">
+          <div className="p-6">
+            <h1 className="text-3xl font-bold mb-4">{product.name}</h1>
+            <Image src={product.image} alt={product.name} className="w-full h-full object-cover" width={640} height={640} />
+            <p className="text-gray-600 mb-4">{product.description}</p>
+            <p className="text-2xl font-bold text-green-600 mb-4">${product.price}</p>
+            <button className="bg-green-500 text-white px-4 py-2 rounded hover:bg-green-600 transition duration-300" onClick={() => { addItemToCart(product) }}>
+              Add to Cart
+            </button>
+          </div>
+          <div className="p-6">
+            <h1 className="text-3xl font-bold mb-4">Comments</h1>
+            {comments.map((comment) => (
+              <div className="bg-grey rounded-lg shadow-md overflow-hidden hover:shadow-lg transition duration-300 gap-6">
+                <div className="p-4">
+                  <h2 className="text-xl font-semibold mb-2">{comment.username}</h2>
+                  <p className="text-gray-600 mb-4">{comment.text}</p>
+                </div>
               </div>
-            </div>
-          ))}
-          {isLoggedin ?
-            (
-              <div id='submit' className="p-6">
-                <form onSubmit={handleSubmit} className="bg-white shadow-md rounded px-8 pt-6 pb-8 mb-4">
-                  <h1 className="text-2xl font-bold mb-6 text-center">Add comment</h1>
-                  <div className="mb-4">
-                    <label className="block text-gray-700 text-sm font-bold mb-2" htmlFor="comment">
-                      Comment
-                    </label>
-                    <textarea
-                      className="shadow appearance-none border rounded w-full py-2 px-3 text-gray-700 leading-tight focus:outline-none focus:shadow-outline"
-                      id="text"
-                      name="text"
-                      value={formComment.text}
-                      onChange={handleChange}
-                      required
-                    ></textarea>
-                  </div>
-                  <div className="flex items-center justify-between">
-                    <button
-                      className="bg-green-500 hover:bg-green-700 text-white font-bold py-2 px-4 rounded focus:outline-none focus:shadow-outline"
-                      type="submit"
-                    >
-                      Submit
-                    </button>
-                  </div>
-
-                </form>
-                <div id='error' className="flex items-center justify-between text-red-500" />
-              </div>
-            ) : (
-              <p className="text-gray-600 mb-4"><br></br>Login to add comments</p>
-            )}
+            ))}
+            {isLoggedin ?
+              (
+                <div id='submit' className="p-6">
+                  <form onSubmit={handleSubmit} className="bg-white shadow-md rounded px-8 pt-6 pb-8 mb-4">
+                    <h1 className="text-2xl font-bold mb-6 text-center">Add comment</h1>
+                    <div className="mb-4">
+                      <label className="block text-gray-700 text-sm font-bold mb-2" htmlFor="comment">
+                        Comment
+                      </label>
+                      <textarea
+                        className="shadow appearance-none border rounded w-full py-2 px-3 text-gray-700 leading-tight focus:outline-none focus:shadow-outline"
+                        id="text"
+                        name="text"
+                        value={formComment.text}
+                        onChange={handleChange}
+                        required
+                      ></textarea>
+                    </div>
+                    <div className="flex items-center justify-between">
+                      <button
+                        className="bg-green-500 hover:bg-green-700 text-white font-bold py-2 px-4 rounded focus:outline-none focus:shadow-outline"
+                        type="submit"
+                      >
+                        Submit
+                      </button>
+                    </div>
+  
+                  </form>
+                  <div id='error' className="flex items-center justify-between text-red-500" />
+                </div>
+              ) : (
+                <p className="text-gray-600 mb-4"><br></br>Login to add comments</p>
+              )}
+          </div>
         </div>
-      </div>
-    </Layout>
+      </Layout>
+     </>
   );
 }
 
